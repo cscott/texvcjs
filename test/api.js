38e01a857b5c944d57c368c63b7c70c7f8c4cbcd
C. Scott Ananian <cscott@cscott.net>
2014-07-21 15:52:43 -0400

    Ensure that function names match longest alpha string.
    In particular `\\write18` should trigger 'unknown TeX function \write'
    rather than getting parsed as `\wr ite18`.

--- test/api.js
+++ test/api.js
@@ -4,15 +4,25 @@ var assert = require('assert');
 var texvcjs = require('../');
 
 describe('API', function() {
-    it('should return success', function() {
+    it('should return success (1)', function() {
 	var result = texvcjs.check('\\sin(x)+{}{}\\cos(x)^2 newcommand');
 	assert.equal(result.status, '+');
 	assert.equal(result.output, '\\sin(x)+{}{}\\cos(x)^{2}newcommand');
     });
-    it('should report undefined functions', function() {
+    it('should return success (2)', function() {
+	var result = texvcjs.check('y=x+2');
+	assert.equal(result.status, '+');
+	assert.equal(result.output, 'y=x+2');
+    });
+    it('should report undefined functions (1)', function() {
 	var result = texvcjs.check('\\foo');
 	assert.equal(result.status, 'F');
 	assert.equal(result.details, '\\foo');
+    });
+    it('should report undefined functions (2)', function() {
+	var result = texvcjs.check('\\write18');
+	assert.equal(result.status, 'F');
+	assert.equal(result.details, '\\write');
     });
     it('should report undefined parser errors', function() {
 	var result = texvcjs.check('^');
