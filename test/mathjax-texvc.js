0c81769dab3ba212c427a5b53bb9048db05c4d5c
physikerwelt <wiki@physikerwelt.de>
2015-08-04 18:58:08 +0200

    Add test cases for mathjax commands
    This adds testcases for all mediwiki specific mathjax
    command for client-side mathjax rendering.

---
+++ test/mathjax-texvc.js
@@ -0,0 +1,27 @@
+"use strict";
+var assert = require('assert');
+var texvcjs = require('../');
+
+var fs = require('fs');
+var path = require('path');
+
+
+describe('Run test for all mathjax-texvc commands:', function () {
+    this.timeout(0);
+    // read test cases
+    var formulae = require('./mathjax-texvc.json');
+    // create a mocha test case for each chunk
+    formulae.forEach(function (testcase) {
+        if(testcase.ignore !== true ) {
+            it(testcase.id+"$"+testcase.input+"$", function () {
+                var result = texvcjs.check(testcase.input);
+                assert.equal(result.output, testcase.texvcjs,
+                    JSON.stringify({
+                        id: testcase.id,
+                        output: result.output,
+                        expected: testcase.texvcjs
+                    }, null, 2));
+            });
+        }
+    });
+});
