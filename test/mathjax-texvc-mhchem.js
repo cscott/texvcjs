b0ee327ee0461fcf9d241c160f85fb2b7eeb01de
Manfred Schäfer <manfred.schaefer@uni.kn>
2017-06-13 15:10:19 +0200

    Started adding tests

---
+++ test/mathjax-texvc-mhchem.js
@@ -0,0 +1,29 @@
+"use strict";
+var assert = require('assert');
+var texvcjs = require('../');
+
+var fs = require('fs');
+var path = require('path');
+
+
+describe('Run test for all mathjax-texvc commands:', function () {
+
+    require('../lib/build-parser');
+    this.timeout(0);
+    // read test cases
+    var formulae = require('./mathjax-texvc-mhchem.json');
+    // create a mocha test case for each chunk
+    formulae.forEach(function (testcase) {
+        if (testcase.ignore !== true) {
+            it(testcase.id+"$"+testcase.input+"$", function () {
+                var result = texvcjs.check(testcase.input,{usemathrm:true, usemhchem:true});
+                assert.equal(result.output, testcase.texvcjs,
+                    JSON.stringify({
+                        id: testcase.id,
+                        output: result.output,
+                        expected: testcase.texvcjs
+                    }, null, 2));
+            });
+        }
+    });
+});
