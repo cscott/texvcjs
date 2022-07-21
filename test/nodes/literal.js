2bdc07eb6987535dac6b65fd3b5d2bcd14edc7a6
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2022-07-21 14:50:12 +0200

    Implement literal class
    Change-Id: Id77d3a63d57718c78f6ce2c39e37886073980f26

---
+++ test/nodes/literal.js
@@ -0,0 +1,25 @@
+const assert = require('assert');
+
+const Literal = require('../../lib/nodes/literal');
+const Node = require('../../lib/nodes/texnode');
+
+describe('Baseclass Node test', function () {
+    it('Should not create an empty literal', function () {
+        assert.throws(()=> new Literal())
+    });
+    it('Should not create an literal with more than one argument', function () {
+        assert.throws(()=> new Literal('a','b'))
+    });
+    it('Should not create a literal with incorrect type', function () {
+        assert.throws(()=> new Literal(new Node()))
+    });
+    it('Should create an literal with only one argument', function () {
+        const l =  new Literal('hello world');
+        assert.strictEqual('hello world', l.render())
+    });
+    it('Should render within node base class', function () {
+        const l = new Literal('hello world');
+        const n = new Node(l);
+        assert.strictEqual('hello world', n.render())
+    });
+});
