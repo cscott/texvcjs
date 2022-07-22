1ebc0a92ce7ec46502dd92148c98657e6b4ff2b7
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2022-07-22 20:26:25 +0200

    Implement matrix node
    Change-Id: Ia69980cfe8bcc1569f301f516b603de20751449d

---
+++ test/nodes/matrix.js
@@ -0,0 +1,40 @@
+const assert = require('assert');
+const Matrix = require('../../lib/nodes/matrix');
+
+const TexNode = require('../../lib/nodes/texnode');
+const TexArray = require('../../lib/nodes/texArray');
+const Literal = require("../../lib/nodes/literal");
+
+describe('Matrix Node test', function () {
+    const sampleMatrix = new Matrix(
+        'align',
+        new TexArray(
+            new TexArray(new Literal('a'))));
+
+    it('Should require two arguments', function () {
+        assert.throws(()=> new Matrix())
+    });
+
+    it('Should require check second argument for nested arrays', function () {
+        assert.throws(()=> new Matrix(
+            'align',
+            new TexArray(
+                new Literal('a'))))
+    });
+
+    it('Should create an instance of TexNode', function () {
+        assert.ok(sampleMatrix instanceof TexNode)
+    });
+
+    it('Should render', function () {
+        assert.strictEqual(
+            '{\\begin{align}a\\end{align}}',
+            sampleMatrix.render())
+    });
+
+    it('Should not create extra curlies', function () {
+        assert.strictEqual(
+            '{\\begin{align}a\\end{align}}',
+            sampleMatrix.inCurlies());
+    });
+});
