f73b0ecb05b3b857fd8ae11e2327d0be3407a401
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2022-07-25 22:19:23 +0200

    Implement boxes
    Change-Id: Id898061c4ae536e055fc99b9e6aba41fdf0bb6fa

---
+++ test/nodes/box.js
@@ -0,0 +1,30 @@
+const assert = require('assert');
+const Literal = require('../../lib/nodes/literal');
+const Box = require('../../lib/nodes/box');
+
+describe('Box Node test', function () {
+
+    it('Should not create an empty Box', function () {
+        assert.throws(()=> new Box())
+    });
+
+    it('Should not create a Box with one argument', function () {
+        assert.throws(()=> new Box('\\hbox'));
+    });
+
+    it('Should not create a Box with incorrect type', function () {
+        assert.throws(()=> new Box('\\hbox', new Literal('a')))
+    });
+
+    it('Should create an basic function', function () {
+        const f =  new Box( '\\hbox', 'a' );
+        assert.strictEqual('{\\hbox {a}}',
+            f.render())
+    });
+
+    it('Should create exactly on set of curlies', function () {
+        const f =  new Box( '\\hbox', 'a' );
+        assert.strictEqual('{\\hbox {a}}',
+            f.inCurlies())
+    });
+});
