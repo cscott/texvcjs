402626a2cea62b84daae5c1b846f5e922bf95c17
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2022-08-11 11:46:22 +0200

    Implement array identifier extraction
    * Only for array
    * Add type checking in constructor
    
    Change-Id: I4ce3a665dba0fd771e6acce544e0641ce993102b

--- test/nodes/texArray.js
+++ test/nodes/texArray.js
@@ -17,8 +17,40 @@ describe('Array Node test', function () {
         const array = new TexArray(new Literal('a'), new Literal('b'));
         assert.strictEqual('ab', array.render())
     });
-    it('Should not exactly one pair of curlies', function () {
-        const n = new TexNode(new TexArray('a') );
+    it('Should create exactly one pair of curlies', function () {
+        const n = new TexNode(new TexArray(new Literal('a')) );
         assert.strictEqual('{a}',n.inCurlies());
+    });
+    it('Should extract identifiers', function () {
+        const n = new TexArray(new Literal('d'));
+        assert.deepEqual(['d'], n.extractIdentifiers());
+    });
+    it('Should extract identifiers from the argument.', function () {
+        const n = new TexArray();
+        assert.deepEqual(['d'], n.extractIdentifiers([new Literal('d')]));
+    });
+    it('Should extract split identifiers', function () {
+        const n = new TexArray(new Literal('a'), new Literal('\''));
+        assert.deepEqual(['a\''], n.extractIdentifiers());
+    });
+    it('Should not confuse integrals and identifiers', function () {
+        const n = new TexArray(new Literal('d'), new Literal('\\int'));
+        assert.deepEqual(['d'], n.extractIdentifiers());
+    });
+    it('Should not confuse integral d with d identifier', function () {
+        const n = new TexArray( new Literal('\\int'), new Literal('d'));
+        assert.deepEqual([], n.extractIdentifiers());
+    });
+    it('Should not confuse upright integral d with d identifier', function () {
+        const n = new TexArray( new Literal('\\int'), new Literal('\\mathrm{d}'));
+        assert.deepEqual([], n.extractIdentifiers());
+    });
+    it('Should extract identifier modifications', function () {
+        const n = new TexArray( new TexNode('') );
+        assert.deepEqual([],n.getModIdent());
+    });
+    it('Should extract subscripts', function () {
+        const n = new TexArray(new TexNode(''));
+        assert.deepEqual([],n.extractSubscripts());
     });
 });
