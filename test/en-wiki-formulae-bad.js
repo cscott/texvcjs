ca9b33d3b5081ae78829af4c65322becb4f4a216
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2022-11-28 00:28:40 +0100

    Restructure and enable en-wiki test cases
    * Simplify en-wiki test case
    * Run them as part of regular CI tasks (cf., T323942)
    * Remove duplicates
    * Remove invalid input
    
    Change-Id: Ifbc8e1851535400cc5670aa8ff750f791accbc98

---
+++ test/en-wiki-formulae-bad.js
@@ -0,0 +1,21 @@
+'use strict';
+const assert = require('assert');
+const texvcjs = require('../');
+
+
+describe('All formulae from en-wiki:', function () {
+    const formulae = require('./en-wiki-formulae-bad.json');
+    Object.keys(formulae).forEach(function (key) {
+        it(formulae[key].substring(0,20) + ' ' + key, () => {
+            const testcase = {
+                inputhash: key,
+                input: formulae[key]
+            }
+            const f = testcase.input;
+            const result = texvcjs.check(f);
+            const good = (result.status === '+');
+            assert.ok(!good, f + ' hash: ' + testcase.inputhash);
+            });
+        });
+});
+
